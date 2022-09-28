2f8aed919bb877f7030658a31e4cdb33b4944afc
Olawale Aro <aro.olawale1@gmail.com>
2022-09-28 13:02:06 +0100

    upgraded mysql to mysql2 to fix auth protocol issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
